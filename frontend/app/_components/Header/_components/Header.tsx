da60405815683777aa5e6871cadc24219c53ad6c
MrHyaena <dolezalmartin131@gmail.com>
2025-06-23 14:34:16 +0200

    Frontened: EsLint error, used a instead of Link

--- frontend/app/_components/Header/_components/Header.tsx
+++ frontend/app/_components/Header/_components/Header.tsx
@@ -12,13 +12,13 @@ export default function Header() {
     return (
       <>
         <div className="w-wrapper md:flex justify-between items-center hidden">
-          <a
+          <Link
             href="/"
             className="flex items-center gap-2 font-semibold font-oswald"
           >
             <BsHouseCheckFill className="text-4xl text-primary" />
             <p className="text-3xl lowercase mt-[-5px] text-textLight">Reas</p>
-          </a>
+          </Link>
           <ul className="flex gap-10 items-center *:not-last:hover:text-primary *:not-last:transition-all *:not-last:ease-in-out font-oswald text-textLight">
             <li>
               <a href="https://www.reas.cz/prodej/nemovitosti">
