76f79840ae34ae2203c3e7d23d3bd505ac85a5e8
MrHyaena <dolezalmartin131@gmail.com>
2025-06-23 12:02:52 +0200

    Frontend: Mobile header
    - Mobile header part is toggled with menu icon
    - Header has Two parts, one for mobile, one for
    desktop

--- frontend/app/_components/Header/_components/Header.tsx
+++ frontend/app/_components/Header/_components/Header.tsx
@@ -1,12 +1,17 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { BsHouseCheckFill } from "react-icons/bs";
+import { TiThMenu } from "react-icons/ti";
 
 type Props = {};
 
 export default function Header({}: Props) {
+  const [menuToggle, setMenuToggle] = useState(false);
+
   return (
-    <div className="w-full  flex items-center justify-center py-4 fixed top-0 px-10">
-      <div className="w-wrapper flex justify-between items-center">
+    <div className="w-full bg-slate-900/90 flex items-center justify-center py-4 fixed top-0 px-10">
+      <div className="w-wrapper md:flex justify-between items-center hidden">
         <a
           href="/"
           className="flex items-center gap-2 font-semibold font-oswald"
@@ -36,6 +41,49 @@ export default function Header({}: Props) {
             </a>
           </li>
         </ul>
+      </div>
+      <div className="flex flex-col gap-10 w-wrapper md:hidden">
+        <div className="flex justify-between">
+          {" "}
+          <a
+            href="/"
+            className="flex items-center gap-2 font-semibold font-oswald"
+          >
+            <BsHouseCheckFill className="text-4xl text-primary" />
+            <p className="text-3xl lowercase mt-[-5px] text-textLight">Reas</p>
+          </a>
+          <button
+            onClick={() => {
+              setMenuToggle(!menuToggle);
+            }}
+          >
+            <TiThMenu className="text-textLight text-2xl" />
+          </button>
+        </div>
+        {menuToggle && (
+          <ul className=" flex flex-col gap-10 items-center *:not-last:hover:text-primary *:not-last:transition-all *:not-last:ease-in-out font-oswald text-textLight">
+            <li>
+              <a href="https://www.reas.cz/prodej/nemovitosti">
+                Nabídka nemovitostí
+              </a>
+            </li>
+            <li>
+              <a href="https://www.reas.cz/realitni-makleri">
+                Přehled realitních makléřů
+              </a>
+            </li>
+            <li>
+              <a href="https://www.reas.cz/kontakt">Kontakt</a>
+            </li>
+            <li>
+              <a href="https://www.reas.cz/prihlasit">
+                <button className="buttonBasics font-medium p-2 px-4 hover:scale-105 ">
+                  uživatelský účet
+                </button>
+              </a>
+            </li>
+          </ul>
+        )}
       </div>
     </div>
   );
