ffcc1e7aa4a307d28aaa44c8c1ad66346fc2ae21
MrHyaena <dolezalmartin131@gmail.com>
2025-06-23 12:04:27 +0200

    Frontend: Mobile header
    - Header has two parts - Desktop and mobile
    - Mobile header is being toggled by button with
    icon
    - Responsiveness made with tailwind

--- frontend/app/_components/Header/_components/Header.tsx
+++ frontend/app/_components/Header/_components/Header.tsx
@@ -9,42 +9,10 @@ type Props = {};
 export default function Header({}: Props) {
   const [menuToggle, setMenuToggle] = useState(false);
 
-  return (
-    <div className="w-full bg-slate-900/90 flex items-center justify-center py-4 fixed top-0 px-10">
-      <div className="w-wrapper md:flex justify-between items-center hidden">
-        <a
-          href="/"
-          className="flex items-center gap-2 font-semibold font-oswald"
-        >
-          <BsHouseCheckFill className="text-4xl text-primary" />
-          <p className="text-3xl lowercase mt-[-5px] text-textLight">Reas</p>
-        </a>
-        <ul className="flex gap-10 items-center *:not-last:hover:text-primary *:not-last:transition-all *:not-last:ease-in-out font-oswald text-textLight">
-          <li>
-            <a href="https://www.reas.cz/prodej/nemovitosti">
-              Nabídka nemovitostí
-            </a>
-          </li>
-          <li>
-            <a href="https://www.reas.cz/realitni-makleri">
-              Přehled realitních makléřů
-            </a>
-          </li>
-          <li>
-            <a href="https://www.reas.cz/kontakt">Kontakt</a>
-          </li>
-          <li>
-            <a href="https://www.reas.cz/prihlasit">
-              <button className="buttonBasics font-medium p-2 px-4 hover:scale-105 ">
-                uživatelský účet
-              </button>
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className="flex flex-col gap-10 w-wrapper md:hidden">
-        <div className="flex justify-between">
-          {" "}
+  function DesktopHeader() {
+    return (
+      <>
+        <div className="w-wrapper md:flex justify-between items-center hidden">
           <a
             href="/"
             className="flex items-center gap-2 font-semibold font-oswald"
@@ -52,16 +20,7 @@ export default function Header({}: Props) {
             <BsHouseCheckFill className="text-4xl text-primary" />
             <p className="text-3xl lowercase mt-[-5px] text-textLight">Reas</p>
           </a>
-          <button
-            onClick={() => {
-              setMenuToggle(!menuToggle);
-            }}
-          >
-            <TiThMenu className="text-textLight text-2xl" />
-          </button>
-        </div>
-        {menuToggle && (
-          <ul className=" flex flex-col gap-10 items-center *:not-last:hover:text-primary *:not-last:transition-all *:not-last:ease-in-out font-oswald text-textLight">
+          <ul className="flex gap-10 items-center *:not-last:hover:text-primary *:not-last:transition-all *:not-last:ease-in-out font-oswald text-textLight">
             <li>
               <a href="https://www.reas.cz/prodej/nemovitosti">
                 Nabídka nemovitostí
@@ -83,8 +42,67 @@ export default function Header({}: Props) {
               </a>
             </li>
           </ul>
-        )}
-      </div>
+        </div>
+      </>
+    );
+  }
+
+  function MobileHeader() {
+    return (
+      <>
+        <div className="flex flex-col gap-10 w-wrapper md:hidden">
+          <div className="flex justify-between">
+            {" "}
+            <a
+              href="/"
+              className="flex items-center gap-2 font-semibold font-oswald"
+            >
+              <BsHouseCheckFill className="text-4xl text-primary" />
+              <p className="text-3xl lowercase mt-[-5px] text-textLight">
+                Reas
+              </p>
+            </a>
+            <button
+              onClick={() => {
+                setMenuToggle(!menuToggle);
+              }}
+            >
+              <TiThMenu className="text-textLight text-2xl" />
+            </button>
+          </div>
+          {menuToggle && (
+            <ul className=" flex flex-col gap-10 items-center *:not-last:hover:text-primary *:not-last:transition-all *:not-last:ease-in-out font-oswald text-textLight">
+              <li>
+                <a href="https://www.reas.cz/prodej/nemovitosti">
+                  Nabídka nemovitostí
+                </a>
+              </li>
+              <li>
+                <a href="https://www.reas.cz/realitni-makleri">
+                  Přehled realitních makléřů
+                </a>
+              </li>
+              <li>
+                <a href="https://www.reas.cz/kontakt">Kontakt</a>
+              </li>
+              <li>
+                <a href="https://www.reas.cz/prihlasit">
+                  <button className="buttonBasics font-medium p-2 px-4 hover:scale-105 ">
+                    uživatelský účet
+                  </button>
+                </a>
+              </li>
+            </ul>
+          )}
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <div className="w-full bg-slate-900/90 flex items-center justify-center py-4 fixed top-0 px-10">
+      <DesktopHeader />
+      <MobileHeader />
     </div>
   );
 }
