473fe7caedc1918b93db78876def20d062ee19b6
MrHyaena <dolezalmartin131@gmail.com>
2025-06-26 12:06:17 +0200

    Frontend: Copy grammar mistake

--- frontend/app/_data/formIntroductionsTexts.ts
+++ frontend/app/_data/formIntroductionsTexts.ts
@@ -3,7 +3,7 @@
 export const formIntroductionTexts: string[] = [
   "Jak to bude probíhat",
   "Formulář má dvě části",
-  "V prvník kroku vyplníte osobní údaje, abychom s vámi měli spojení",
+  "V prvním kroku vyplníte osobní údaje, abychom s vámi měli spojení",
   "Druhá část spočívá v základním zařazení nemovitosti do kategorie a lokality",
   "Poté už si jen všechno překontrolujete v souhrnu a formulář odešlete",
   "Základy budou hotové a váš účet se začne tvořit",
