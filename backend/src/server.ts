f703c97af43d5a5129de32fc81bf2475cad9ebdc
MrHyaena <dolezalmartin131@gmail.com>
2025-07-09 10:06:53 +0200

    DevOps: Learning to start service on remote server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,6 +7,8 @@ dotenv.config();
 //URI for connecting to database
 const mongoUri = process.env.MONGO_URI;
 
+const PORT = Number(process.env.PORT);
+
 //Server starting process
 if (mongoUri != undefined) {
   mongoose
@@ -15,7 +17,7 @@ if (mongoUri != undefined) {
       console.log(`connection to MongoDB successful`);
 
       //Starting listening to server after connecting to db
-      app.listen(process.env.PORT, () =>
+      app.listen(PORT, "0.0.0.0", () =>
         console.log(`listening to port ${process.env.PORT}`)
       );
     })
